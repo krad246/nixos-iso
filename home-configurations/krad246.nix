b62893ab570dc3ba9ebf04559ee369b6bb0c0500
Keerthi Radhakrishnan <krad246@gmail.com>
2024-03-04 01:21:43 -0500

    Add steam

--- home-configurations/krad246.nix
+++ home-configurations/krad246.nix
@@ -1,7 +1,13 @@
-{ezModules, ...}: {
+{
+  ezModules,
+  pkgs,
+  ...
+}: {
   imports = with ezModules;
     [
       ./krad246-cli.nix
     ]
     ++ [discord spotify vscode];
+
+  home.packages = with pkgs; [steam];
 }
