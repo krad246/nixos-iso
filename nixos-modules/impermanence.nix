d6fb43e8a8d17344dde1daaee6e71d2d6e617f92
Keerthi Radhakrishnan <krad246@gmail.com>
2024-03-08 19:06:12 +0000

    Get impermanence module to be called correctly

---
+++ nixos-modules/impermanence.nix
@@ -0,0 +1,23 @@
+{
+  environment.persistence."/nix/persist" = {
+    hideMounts = true;
+    directories = [
+      "/var/log"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+      "/etc/NetworkManager/system-connections"
+      {
+        directory = "/var/lib/colord";
+        user = "colord";
+        group = "colord";
+        mode = "u=rwx,g=rx,o=";
+      }
+    ];
+    files = [
+      "/etc/machine-id"
+    ];
+  };
+
+  fileSystems."/nix/persist".neededForBoot = true;
+}
