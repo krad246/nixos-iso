385a546abdb51313a8f3d534c961de4a5351ac1b
Keerthi Radhakrishnan <krad246@gmail.com>
2024-03-08 19:19:52 +0000

    Add temporary hardware-configuration to impermanent setup

--- nixos-configurations/impermanent-pantheon/default.nix
+++ nixos-configurations/impermanent-pantheon/default.nix
@@ -1,5 +1,5 @@
 {lib, ...}: {
-  imports = [./nixos-pantheon.nix ./platform.nix ./filesystems.nix];
+  imports = [./nixos-pantheon.nix ./platform.nix ./filesystems.nix ./hardware-configuration.nix];
 
   networking.hostName = "impermanent-pantheon";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
