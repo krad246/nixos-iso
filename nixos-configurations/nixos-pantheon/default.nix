b9436cbc39d67efb5877c62fc2d2ecee015e9db5
Keerthi Radhakrishnan <krad246@gmail.com>
2024-03-08 14:41:13 +0000

    Bake home configuration into another NixOS image

--- nixos-configurations/nixos-pantheon/default.nix
+++ nixos-configurations/nixos-pantheon/default.nix
@@ -1,5 +1,19 @@
-{lib, ...}: {
-  imports = [./nixos-pantheon.nix ./platform.nix];
+{
+  inputs,
+  lib,
+  ...
+}: let
+  inherit (inputs) home-manager;
+in {
+  imports =
+    [./nixos-pantheon.nix ./platform.nix]
+    ++ [
+      home-manager.nixosModule
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.users.krad246 = import ../../home-configurations/krad246.nix;
+      }
+    ];
 
   networking.hostName = "nixos-pantheon";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
