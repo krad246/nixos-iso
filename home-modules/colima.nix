b2d2b0844f21a2698d0e03208029b2ee1154230b
Keerthi Radhakrishnan <krad246@gmail.com>
2024-03-12 14:43:10 -0400

    Attempt to autostart Colima as systemd service

--- home-modules/colima.nix
+++ home-modules/colima.nix
@@ -1,3 +1,21 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [colima docker];
+  systemd.user.services.colima-intel-autostart = {
+    Unit = {
+      Description = "Push nix store changes to attic binary cache.";
+    };
+    Install = {
+      WantedBy = ["default.target"];
+    };
+    Service = {
+      ExecStart = "${pkgs.writeShellScript "colima-intel-start" ''
+        #!/run/current-system/sw/bin/bash
+        ${lib.getExe pkgs.colima} start -p intel --disk
+      ''}";
+    };
+  };
 }
