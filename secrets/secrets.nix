75cee5d467db9eaaee8d58fe89dc6178e789f112
Keerthi Radhakrishnan <krad246@gmail.com>
2024-03-23 09:30:47 -0400

    Figure out how to deploy some initial secrets to a few machines and separate identities

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,13 +1,20 @@
 let
+  # System / privileged identity per host.
   root = {
     nixbook-air = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7/zqo4VsyDk/760jQaV6GLJ40E4+ETMJPexWmsQOPc";
   };
 
+  # User identities per host.
   krad246 = {
     immutable-gnome = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGKd83MiKefYk702WTPhimblIQGsZKcNDlnhwuYjqDgo";
     nixos-wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk51a/d6J53YyN4N6Q5gstkup07dPlQzIS/QiBzoMzg";
     nixbook-air = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxG+GLvLuIXhSskofvux2kvRBSDECBf6G3+9rUguER1";
   };
+  # Secrets listed below list the identities allowed to unlock them.
 in {
-  "gh.age".publicKeys = (with krad246; [immutable-gnome nixos-wsl nixbook-air]) ++ (with root; [nixbook-air]);
+  # Github CLI login in userspace
+  "gh.age".publicKeys = with krad246; [immutable-gnome nixos-wsl nixbook-air];
+
+  # Cryptsetup password file
+  "cryptsetup.age".publicKeys = with root; [nixbook-air];
 }
